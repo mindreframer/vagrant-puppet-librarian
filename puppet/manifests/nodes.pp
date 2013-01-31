7c0e47fc8b465e3d466ea444c8ada94ea0e1a0e5
Roman Heinrich <roman.heinrich@gmail.com>
2013-01-31 20:45:39 +0200

    no installation for now...

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -1,3 +1,13 @@
 node default {
   class{"rvm": }
+  #  ->
+  # http://master.dl.sourceforge.net/project/rvmbinaries/ubuntu/12.04/x86_64/ruby-1.9.3-p374-turbo.tar.bz2
+  # rvm_system_ruby {
+  #   'ruby-1.9.3-p374-turbo':
+  #     ensure      => present,
+  #     default_use => false;
+  #   'ree-1.8.7-2012.02':
+  #     ensure      => present,
+  #     default_use => false;
+  # }
 }
